aa7dd10258d137b048b3123eb9e083f52f09e6a2
Reppas <James.Reppas@rjuhsd.int>
2022-01-25 12:47:26 -0800

    Ask user birth info

--- birthday.py
+++ birthday.py
@@ -1,2 +1,7 @@
-for i in range(20):
-    print("WELCOME.")
+import datetime
+
+print("Welcome")
+m = input("Enter your birth month: ")
+d = input("Enter your birth day: ")
+y = input("Enter your birth year: ")
+print("Your birthday is: " + m + "-" + d + "-" + y)
